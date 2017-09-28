4971b6f425970350be13b29cd12fd3aa50e80bdb
mohamed <mohamed.bouhamed@lcdlv.com>
2017-09-28 15:20:59 +0200

    refacto

--- src/main/java/domain/XspeedltParser.java
+++ src/main/java/domain/XspeedltParser.java
@@ -1,29 +1,23 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class XspeedltParser {
     public static final String SEPARATOR = "/";
-    StringBuilder productsList = new StringBuilder ( );
+    List<String> productsList = new ArrayList<> ( );
 
     public XspeedltParser(String products) {
         if ( !products.isEmpty ( ) ) {
             final int length = products.length ( );
             for (int element = 0; element < length; element++) {
-                productsList.append (createNewProduct (products, length, element));
+                this.productsList.add (String.valueOf (products.charAt (element)));
             }
         }
     }
 
-    private String createNewProduct(String products, int length, int element) {
-        final String product = String.valueOf (products.charAt (element));
-        final String separatorBetweenProducts = addSeparator (element, length);
-        return product + separatorBetweenProducts;
-    }
-
-    private String addSeparator(int element, int length) {
-        return element < length - 1 ? SEPARATOR : "";
-    }
-
-    public String getProducts() {
-        return productsList.toString ( );
+    public List<String> getProducts() {
+        return Collections.unmodifiableList (productsList);
     }
 }
--- src/test/java/domain/XspeedltParserTest.java
+++ src/test/java/domain/XspeedltParserTest.java
@@ -3,22 +3,28 @@ package domain;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Collections;
+
 public class XspeedltParserTest {
     @Test
-    public void should_return_empty_list_when_no_product() {
+    public void should_return_empty_when_no_product() {
         final XspeedltParser xspeedltParser = new XspeedltParser ("");
-        Assert.assertEquals ("", xspeedltParser.getProducts ( ));
+        Assert.assertEquals (Collections.EMPTY_LIST, xspeedltParser.getProducts ( ));
     }
 
     @Test
-    public void should_return_empty_list_when_one_product() {
+    public void should_return_1_list_when_one_product() {
         final XspeedltParser xspeedltParser = new XspeedltParser ("1");
-        Assert.assertEquals ("1", xspeedltParser.getProducts ( ));
+        Assert.assertEquals ("1", xspeedltParser.getProducts ( ).get (0));
     }
 
     @Test
-    public void should_return_empty_list_when_many_product() {
+    public void should_return_string_element_when_many_products() {
         final XspeedltParser xspeedltParser = new XspeedltParser ("12984");
-        Assert.assertEquals ("1/2/9/8/4", xspeedltParser.getProducts ( ));
+        Assert.assertEquals ("1", xspeedltParser.getProducts ( ).get (0));
+        Assert.assertEquals ("2", xspeedltParser.getProducts ( ).get (1));
+        Assert.assertEquals ("9", xspeedltParser.getProducts ( ).get (2));
+        Assert.assertEquals ("8", xspeedltParser.getProducts ( ).get (3));
+        Assert.assertEquals ("4", xspeedltParser.getProducts ( ).get (4));
     }
 }

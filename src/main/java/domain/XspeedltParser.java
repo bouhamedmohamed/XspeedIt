19e0b893268cbb102561dadb0f91f1564314324a
mohamed <mohamed.bouhamed@lcdlv.com>
2017-09-28 14:52:43 +0200

    refacto

--- src/main/java/domain/XspeedltParser.java
+++ src/main/java/domain/XspeedltParser.java
@@ -6,15 +6,21 @@ public class XspeedltParser {
 
     public XspeedltParser(String products) {
         if ( !products.isEmpty ( ) ) {
-            for (int i = 0; i < products.length ( ); i++) {
-                productsList.append (String.valueOf (products.charAt (i)) + SEPARATOR);
+            final int length = products.length ( );
+            for (int element = 0; element < length; element++) {
+                productsList.append (createNewProduct (products, length, element));
             }
-            deleteLastSeparator ( );
         }
     }
 
-    private void deleteLastSeparator() {
-        productsList.deleteCharAt (productsList.length ( ) - 1);
+    private String createNewProduct(String products, int length, int element) {
+        final String product = String.valueOf (products.charAt (element));
+        final String separatorBetweenProducts = addSeparator (element, length);
+        return product + separatorBetweenProducts;
+    }
+
+    private String addSeparator(int element, int length) {
+        return element < length - 1 ? SEPARATOR : "";
     }
 
     public String getProducts() {

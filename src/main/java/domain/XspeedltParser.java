7eb4d757b9d7d030b4447de73394b4eb08a64d92
mohamed <mohamed.bouhamed@lcdlv.com>
2017-09-28 14:17:41 +0200

    one product to parse

--- src/main/java/domain/XspeedltParser.java
+++ src/main/java/domain/XspeedltParser.java
@@ -1,13 +1,19 @@
 package domain;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class XspeedltParser {
-    public XspeedltParser() {
+    List productsList = new ArrayList ( );
+
+    public XspeedltParser(String products) {
+        if ( !products.isEmpty ( ) )
+            productsList.add ("1");
+
     }
 
     public List getProducts() {
-        return Collections.EMPTY_LIST;
+        return Collections.unmodifiableList (productsList);
     }
 }

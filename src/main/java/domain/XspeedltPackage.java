00ada7f019edb6e5528857b93e910b7b8682e8fe
mohamed <mohamed.bouhamed@lcdlv.com>
2017-09-29 15:05:28 +0200

    raise excpetion when we dont have enough capacity in the package

--- src/main/java/domain/XspeedltPackage.java
+++ src/main/java/domain/XspeedltPackage.java
@@ -4,13 +4,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class XspeedltPackage {
-    List products = new ArrayList ( );
+    public static final int MAX_WEIGHT = 10;
+    List<Integer> products = new ArrayList ( );
 
     public List getProductsInside() {
         return products;
     }
 
     public void addProduct(int product) {
-        products.add (product);
+        if ( getPackageCapacity ( ) >= product )
+            products.add (product);
+        else
+            throw new IllegalArgumentException ( );
+    }
+
+    private int getPackageCapacity() {
+        return MAX_WEIGHT - products.stream ( ).mapToInt (i -> i).sum ( );
     }
 }
--- src/test/java/domain/XspeedltPackageTest.java
+++ src/test/java/domain/XspeedltPackageTest.java
@@ -3,13 +3,23 @@ package domain;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.IllformedLocaleException;
+
 public class XspeedltPackageTest {
     @Test
-
-    public void should_add_one_products_below_10_weight() {
+    public void should_add_two_products_below_10_weight() {
         final XspeedltPackage xspeedltPackage = new XspeedltPackage ( );
-        xspeedltPackage.addProduct(2);
-        xspeedltPackage.addProduct(6);
-        Assert.assertEquals (2, xspeedltPackage.getProductsInside().size ());
+        xspeedltPackage.addProduct (2);
+        xspeedltPackage.addProduct (8);
+        Assert.assertEquals (2, xspeedltPackage.getProductsInside ( ).size ( ));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void should_add_three_products_but_just_below_10_weight() {
+        final XspeedltPackage xspeedltPackage = new XspeedltPackage ( );
+        xspeedltPackage.addProduct (2);
+        xspeedltPackage.addProduct (6);
+        xspeedltPackage.addProduct (6);
+        Assert.assertEquals (2, xspeedltPackage.getProductsInside ( ).size ( ));
     }
 }
